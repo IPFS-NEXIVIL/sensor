82e44bf3c2b0702e09c933f46d5e96018bf6a1f1
kdb621 <liradb2000@live.co.kr>
2023-10-23 02:29:26 +0900

    support async

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@
 // mod kxtj3_accelerometer;
 // use i2cdev::core::*;
 
+use tokio::signal;
 use cpal::traits::StreamTrait;
 
 mod mic;
 
-// #[tokio::main]
-fn main() {
+#[tokio::main]
+async fn main() {
     let st = mic::mic();
     st.play().expect("unable to play stream");
+    loop {
+        tokio::select! {
+        _ = signal::ctrl_c() => {
+            println!("SHUTDOWN");
+            // cancel_token.notify_one();
+            break
+        }}
+    }
 }
