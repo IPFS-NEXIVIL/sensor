54cc9470fe9681cc2a4e187c14a576b5745f3f3e
kdb621 <liradb2000@live.co.kr>
2023-10-23 02:48:26 +0900

    Add rand generator;

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,41 @@
 // mod kxtj3_accelerometer;
 // use i2cdev::core::*;
 
+use std::{sync::Arc, time::Duration};
+
+// use cpal::traits::StreamTrait;
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use tokio::signal;
-use cpal::traits::StreamTrait;
 
 mod mic;
 
 #[tokio::main]
 async fn main() {
-    let st = mic::mic();
-    st.play().expect("unable to play stream");
+    // let st = mic::mic();
+    // st.play().expect("unable to play stream");
+    let client = Arc::new(reqwest::blocking::Client::new());
+
+    tokio::spawn({
+        println!("STARTED");
+        let mut rng = {
+            let rng = rand::thread_rng();
+            StdRng::from_rng(rng).unwrap()
+        };
+        let _client = client.clone();
+
+        async move {
+            loop {
+                let _ = client
+                    .clone()
+                    .post("http://10.42.0.1:3000")
+                    .body(format!("{}", rng.gen::<f64>().to_string()))
+                    .send();
+                tokio::time::sleep(Duration::from_millis(700)).await;
+            }
+        }
+    });
+    tokio::task::yield_now().await;
+
     loop {
         tokio::select! {
         _ = signal::ctrl_c() => {
